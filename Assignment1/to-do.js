a95935d00b1b704a2c5744b6618ca745c1205aea
Apinya_LENOVO <apinya.chaiw@ku.th>
2021-07-23 15:32:00 +0700

    add button Like Finish!

--- Assignment1/to-do.js
+++ Assignment1/to-do.js
@@ -6,31 +6,19 @@ function removeTodo(event) {
   event.target.parentNode.remove();
 }
 
-function checkTodo(event) {
-    if (event.target.checked == true){
-        event.target.parentNode.style.color = "red";
-    }
-    else{
-        event.target.parentNode.style.color = "black";
-    }
-  }
-let count = 0;
+/*let count = 0;
 function likeTodo() {
   count += 1;
-  document.querySelector('.like-todo').innerHTML = like;  
-}
-
-function getLike() {
-  likeTodo();
-}
-
+  //document.querySelector('.like-todo').innerHTML = like;*/
 function addTodo() {
   // todo
   const todo = document.createElement('div');
   todo.classList.add('todo');
-
+  //const inputs = document.querySelectorAll('input');
+  //inputs.forEach(input => input.value = '');
+  
   // text
-  const text = document.createElement('h1');
+  const text = document.createElement('line-todo');
   text.classList.add('line-todo')
   text.innerHTML = todoTitle.value;
 
@@ -41,23 +29,33 @@ function addTodo() {
   button.innerHTML = 'ลบ';
   button.addEventListener('click', removeTodo);
   
-  const button2 = document.createElement('input');
+  /*const button2 = document.createElement('input');
   button2.type = 'checkbox';
   button2.classList.add('line-todo');
   button2.addEventListener('click', checkTodo);
-  // text, button => todo
+  // text, button => todo*/
 
-  const button3 = document.createElement('button');
-  button3.type = 'button';
-  button3.classList.add('like-todo');
-  button3.innerHTML = 'like '+ count;
-  button3.addEventListener('click', likeTodo);
+  const button2 = document.createElement('button');
+  button2.type = 'button';
+  button2.classList.add('like-todo');
+  button2.innerText = 'Like ' ;
+  count = 0;
+  button2.onclick = function() {
+    count += 1;
+  button2.innerHTML = 'Like '+ count;
+  }
   
-  todo.append(text, button, button2, button3);
+
+  //button2.addEventListener('click', likeTodo);
+  
+  todo.append(text, button, button2);
 
   // todo => todo list
   todoList.prepend(todo);
 
 }
 
-addTodoButton.addEventListener('click', addTodo);
+addTodoButton.addEventListener('click', addTodo)
+//const inputs = document.querySelectorAll('input');
+//inputs.forEach(input => input.value = '');
+//addTodoButton.addEventListener(input => input.value = "");
